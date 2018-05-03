fa142285642cf04dcaa2e9d308c9225ece0d8d34
Rhenan Konrad <rhenan_konrad@hotmail.com>
2018-05-03 16:52:23 -0300

    Criação do repositório

---
+++ js/meu-arquivo.js
@@ -0,0 +1,34 @@
+function calcIMC() {
+    var imc = 0;
+
+    var peso = parseInt(document.getElementById('pesoid').value, 10);
+    var altura = parseFloat(document.getElementById('alturaid').value, 10);
+
+
+    var imc = parseFloat((peso / (altura * altura)).toFixed(1));
+
+    if (imc > 0 && imc< 20) {
+        document.getElementById("alerta").innerHTML= "Você está abaixo do peso!";
+        document.getElementById("txtrect").innerHTML=imc;
+        document.getElementById("msg").innerHTML = "Magresa: menor que 20";
+    }
+    else if (imc >= 20 && imc < 30) {
+        document.getElementById("alerta").innerHTML= "Você está com o peso normal!";
+        document.getElementById("txtrect").innerHTML=imc;
+        document.getElementById("msg").innerHTML = "Normal: entre 20 e 30";
+    }
+
+    else if (imc >= 30 && imc < 35) {
+        document.getElementById("alerta").innerHTML= "Você está com sobrepeso";
+        document.getElementById("txtrect").innerHTML=imc;
+        document.getElementById("msg").innerHTML = "Sobrepeso: entre 30 e 35";
+    }
+
+    else if (imc > 35) {
+        document.getElementById("alerta").innerHTML= "Você está com obesidade!";
+        document.getElementById("txtrect").innerHTML=imc;
+        document.getElementById("msg").innerHTML = "Obesidade: acima de 35";
+    }
+
+
+}
